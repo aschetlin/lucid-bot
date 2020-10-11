f75e4b2f1722cc64ed88f527cb2c58233c304c9c
Viargentum <viargentummc@gmail.com>
2020-10-10 22:38:01 -0400

    added requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from lucid_bot.bot import bot
 from lucid_bot.config import config
-import lucid_bot.commands
-from lucid_bot.commands import moderation
+import lucid_bot.commands # noqa
+from lucid_bot.commands import moderation # noqa
 
 bot.run(config["token"])
